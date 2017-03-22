ee18f6c8a84f3a5d3e881433c61502dfc62346fc
ElenaLaufeyson <elena_petrova98@mail.ru>
2017-03-22 22:28:41 +0300

    Исправила замечания

--- src/Ciphering/Main.java
+++ src/Ciphering/Main.java
@@ -8,34 +8,38 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        CheckArg check = new CheckArg();
-        Data dat = check.resultcheck(args);
-        Ciphxor coder = new Ciphxor(dat.getKey());
-        FileInputStream filein = null;
-        FileOutputStream fileout = null;
+        CheckArg check = new CheckArg(args);
+        convert(check.getKey(), check.getInputName(), check.getOutputName());
+    }
+
+    public static void convert (String key, String inputName, String outputName) {
+        CiphXor coder = new CiphXor(key);
+        FileInputStream fileIn = null;
+        FileOutputStream fileOut = null;
         int max = 100; //по сколько считываем байт
         int lengh = 0;
         byte text[] = new byte[max];
         byte cut[];
         try {
-            filein = new FileInputStream(dat.getInputname());
-            fileout = new FileOutputStream(dat.getOutputname());
-            while ((lengh = filein.read(text))!= -1) {
+            fileIn = new FileInputStream(inputName);
+            fileOut = new FileOutputStream(outputName);
+            while ((lengh = fileIn.read(text))!= -1) {
                 if (lengh < max) {
                     cut = new byte[lengh];
                     System.arraycopy(text, 0, cut, 0, lengh);
-                    fileout.write(coder.xorCoder(cut));
+                    fileOut.write(coder.xorCoder(cut));
                 }
-                else fileout.write(coder.xorCoder(text));
+                else fileOut.write(coder.xorCoder(text));
             }
-            filein.close();
-            fileout.close();
+            fileIn.close();
+            fileOut.close();
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
             System.exit(1);
         } catch (IOException e) {
-                System.out.println(e.getMessage());
-                System.exit(2);
+            System.out.println(e.getMessage());
+            System.exit(2);
         }
     }
 }
+
