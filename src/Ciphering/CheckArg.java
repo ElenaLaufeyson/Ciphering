f2abf5ac23f19389582bffe5121be9bb86910977
ElenaLaufeyson <elena_petrova98@mail.ru>
2017-03-22 22:29:40 +0300

    Исправила замечания

--- src/Ciphering/CheckArg.java
+++ src/Ciphering/CheckArg.java
@@ -5,20 +5,16 @@ import java.util.regex.Pattern;
 
 public class CheckArg {
 
-    private Boolean check16Syst(String args1) {
-        Pattern check = Pattern.compile("^[a-fA-F0-9]*$");
-        Matcher match = check.matcher(args1);
-        return match.matches();
-    }
+    private String key, inputName, outputName;
 
-    public Data resultcheck(String []args) {
+    public CheckArg(String []args) {
         if (args.length < 3)
             throw new IllegalArgumentException("Command Line: [-c key] [-d key] " +
                     "inputname.txt [-o outputname.txt");
         if (!args[0].equals("-c") && !args[0].equals("-d"))
             throw new IllegalArgumentException("Expected key -c or -d");
-        if (!check16Syst(args[1]))
-            throw new IllegalArgumentException("Wrong key - not hexadecimal number");
+        if (!check16Syst(args[1]) || args[1].length()%2 != 0)
+            throw new IllegalArgumentException("Wrong key - not hexadecimal number or wrong byte");
         if (!(new File(args[2]).exists()))
             throw new IllegalArgumentException("Input file doesn't exist");
         String outputName = "";
@@ -36,7 +32,26 @@ public class CheckArg {
                 throw new IllegalArgumentException("Couldn't find the name of output file");
             outputName = args[4];
         }
-        return new Data(args[1], args[2], outputName);
+        this.key = args[1];
+        this.inputName = args[2];
+        this.outputName = outputName;
+    }
+
+
+    private Boolean check16Syst(String args1) {
+        Pattern check = Pattern.compile("^[a-fA-F0-9]*$");
+        Matcher match = check.matcher(args1);
+        return match.matches();
+    }
+
+    public String getKey() {return this.key; }
+
+    public String getInputName() {
+        return this.inputName;
+    }
+
+    public String getOutputName() {
+        return this.outputName;
     }
 
 }
