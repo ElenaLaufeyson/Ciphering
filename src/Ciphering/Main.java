47f110e7ebb4bd5c7d725fd369a7b5bd461046b5
ElenaLaufeyson <elena_petrova98@mail.ru>
2017-03-30 22:26:50 +0300

    Update Main.java

--- src/Ciphering/Main.java
+++ src/Ciphering/Main.java
@@ -3,8 +3,10 @@ package Ciphering;
 public class Main {
 
     public static void main(String[] args) {
-        CheckArg check = new CheckArg(args);
+        CheckArg check = new CheckArg();
+        check.ReadCommLine(args);
         FileCoder code = new FileCoder(check.getKey(), check.getInputName(), check.getOutputName());
         code.convert();
     }
 }
+
