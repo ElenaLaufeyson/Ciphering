309ea68f343a650c28dad190857f4d3b31c27fa1
ElenaLaufeyson <elena_petrova98@mail.ru>
2017-03-30 22:27:41 +0300

    Update CheckArg.java

--- src/Ciphering/CheckArg.java
+++ src/Ciphering/CheckArg.java
@@ -1,42 +1,41 @@
 package Ciphering;
+import org.kohsuke.args4j.*;
+
 import java.io.File;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class CheckArg {
 
-    private String key, inputName, outputName;
+    @Option(name = "-c", aliases = {"-d"}, metaVar = "key", required = true,
+            usage = "Set connection coding/decoding")
+    private String key;
 
-    public CheckArg(String []args) {
-        if (args.length < 3)
+    @Option(name = "-o", metaVar = "outputName", usage = "Output file coding")
+    private String outputName;
+
+    @Argument(required = true, metaVar = "inputName", usage = "Input file name")
+    private String inputName;
+
+    public void ReadCommLine(String []args) {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+            if (!check16Syst(key))
+                throw new IllegalArgumentException("Wrong key - not hexadecimal number or wrong byte");
+            if (!(new File(inputName).exists()))
+                throw new IllegalArgumentException("Input file doesn't exist");
+            if (outputName == null) {
+                String s[] = inputName.split("\\.");
+                StringBuilder out = new StringBuilder();
+                out.append(s[0]);
+                out.append("_out.txt");
+                outputName = out.toString();
+            }
+        } catch (CmdLineException e) {
             throw new IllegalArgumentException("Command Line: [-c key] [-d key] " +
-                    "inputname.txt [-o outputname.txt");
-        if (!args[0].equals("-c") && !args[0].equals("-d"))
-            throw new IllegalArgumentException("Expected key -c or -d");
-        if (!check16Syst(args[1]))
-            throw new IllegalArgumentException("Wrong key - not hexadecimal number or wrong byte");
-        if (!(new File(args[2]).exists()))
-            throw new IllegalArgumentException("Input file doesn't exist");
-        String outputName = "";
-        if (args.length == 3) {
-            String s[] = args[2].split("\\.");
-            StringBuilder out = new StringBuilder();
-            out.append(s[0]);
-            out.append("_out.txt");
-            outputName = out.toString();
+                    "inputname.txt [-o] outputname.txt");
         }
-        else {
-            if (!(args[3].equals("-o")))
-                throw new IllegalArgumentException("Incorrect format. Expected -o");
-            if (args.length < 5)
-                throw new IllegalArgumentException("Couldn't find the name of output file");
-            outputName = args[4];
-        }
-        this.key = args[1];
-        this.inputName = args[2];
-        this.outputName = outputName;
     }
-
 
     private Boolean check16Syst(String args1) {
         Pattern check = Pattern.compile("^(([a-fA-F0-9]{2})*)$");
