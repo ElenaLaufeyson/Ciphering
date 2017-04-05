bc78224a58a413fc54711390e5fe8ca8409ec24d
ElenaLaufeyson <elena_petrova98@mail.ru>
2017-04-05 21:25:47 +0300

    Исправила последние замечания

--- src/Ciphering/Main.java
+++ src/Ciphering/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
         CheckArg check = new CheckArg();
         check.ReadCommLine(args);
-        FileCoder code = new FileCoder(check.getKey(), check.getInputName(), check.getOutputName());
-        code.convert();
+        FileCoder code = new FileCoder(check.getKey());
+        code.convert(check.getInputName(), check.getOutputName());
     }
 }
-
